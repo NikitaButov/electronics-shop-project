5e4cd8c0ce66cdc7588eadc54b9b8c48b0942a16
Nikita Butov <butov3084@gmail.com>
2023-08-09 16:47:17 +0500

    Добавил классы и тесты к функциям, так-же обновил .gitignore и сощдал зависимость на проверку тестов

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item("Test item", 10, 5)
+    assert item.calculate_total_price() == 50
+
+
+def test_apply_discount():
+    item = Item("Test item", 10, 5)
+    item.apply_discount()
+    assert item.price == 10 * item.__class__.pay_rate
